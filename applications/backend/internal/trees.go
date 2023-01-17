d917c9336a27f53273ec6ec15b561b139b1b1f07
Forest Eckhardt <forest.eckhardt@gmail.com>
2023-01-17 13:59:02 -0500

    Updates data type to use finalized data set

--- applications/backend/internal/trees.go
+++ applications/backend/internal/trees.go
@@ -1,18 +1,30 @@
 package internal
 
 type BackendTree struct {
-	TreeID    string  `json:"tree_id"`
-	TreeDbh   int     `json:"tree_dbh"`
-	SpcLatin  string  `json:"spc_latin" `
-	SpcCommon string  `json:"spc_common"`
-	Latitude  float64 `json:"latitude"`
-	Longitude float64 `json:"longitude"`
+	TreeID       string  `json:"tree_id"`
+	SpeciesID    int     `json:"species_id"`
+	Name         string  `json:"name"`
+	Diameter     int     `json:"diameter"`
+	SpcLatin     string  `json:"spc_latin"`
+	SpcCommon    string  `json:"spc_common"`
+	Introverted  bool    `json:"introverted"`
+	HasGuards    bool    `json:"has_guards"`
+	RootProblems bool    `json:"root_problems"`
+	HasLights    bool    `json:"has_lights"`
+	HasShoes     bool    `json:"has_shoes"`
+	Address      string  `json:"address"`
+	Zipcode      int     `json:"zipcode"`
+	Neighborhood string  `json:"neighborhood"`
+	Latitude     float64 `json:"latitude"`
+	Longitude    float64 `json:"longitude"`
 }
 
 type FrontendTree struct {
 	TreeID    string  `json:"tree_id"`
+	SpeciesID int     `json:"species_id"`
 	SpcLatin  string  `json:"spc_latin" `
 	SpcCommon string  `json:"spc_common"`
+	Name      string  `json:"name"`
 	Latitude  float64 `json:"latitude"`
 	Longitude float64 `json:"longitude"`
 
@@ -22,20 +34,38 @@ type FrontendTree struct {
 }
 
 type Features struct {
-	Diameter int `json:"diameter"`
+	Diameter     int    `json:"diameter"`
+	Introverted  bool   `json:"introverted"`
+	HasGuards    bool   `json:"has_guards"`
+	RootProblems bool   `json:"root_problems"`
+	HasLights    bool   `json:"has_lights"`
+	HasShoes     bool   `json:"has_shoes"`
+	Address      string `json:"address"`
+	Zipcode      int    `json:"zipcode"`
+	Neighborhood string `json:"neighborhood"`
 }
 
 func (bt BackendTree) MakeFront(selected bool, bio string) FrontendTree {
 	return FrontendTree{
 		TreeID:    bt.TreeID,
+		SpeciesID: bt.SpeciesID,
 		SpcLatin:  bt.SpcLatin,
 		SpcCommon: bt.SpcCommon,
+		Name:      bt.Name,
 		Latitude:  bt.Latitude,
 		Longitude: bt.Longitude,
 		Selected:  selected,
 		Bio:       bio,
 		Features: Features{
-			Diameter: bt.TreeDbh,
+			Diameter:     bt.Diameter,
+			Introverted:  bt.Introverted,
+			HasGuards:    bt.HasGuards,
+			RootProblems: bt.RootProblems,
+			HasLights:    bt.HasLights,
+			HasShoes:     bt.HasShoes,
+			Address:      bt.Address,
+			Zipcode:      bt.Zipcode,
+			Neighborhood: bt.Neighborhood,
 		},
 	}
 }
