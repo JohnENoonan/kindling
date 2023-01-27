2d1c91057a55cd668e78b1269acf74cec4c73567
johnenoonan <noonanjohn21@gmail.com>
2023-01-26 23:45:47 -0500

    get google images and start processing area data

--- applications/touch/app/data/dataExt.py
+++ applications/touch/app/data/dataExt.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 headers = {
 	'Content-Type': 'application/x-www-form-urlencoded',
@@ -14,6 +15,8 @@ class DataExt:
 		self.raduisOp = op("/app/scene_app/MAP/area_radius")
 		self.pinpointOp = op("/app/scene_app/MAP/pin_point")
 
+		self.tree_table = op("trees_edit")
+
 
 	def QueryArea(self, lat, lon, rad):
 		params = {
@@ -24,11 +27,36 @@ class DataExt:
 		response = requests.get(self.areaUrl, params=params)
 		return response.json()
 
+	def processResponse(self, data):
+		"""
+		Process the API response of querying the area. Data must be a list of dicts
+		"""
+		# get keys
+		keys = list(data[0].keys())
+		keys.remove("features")
+		keys = ['local_id'] + keys
+		features_keys = list(data[0]["features"].keys())
+		# set up tables
+		self.tree_table.clear()
+		self.tree_table.appendRow(keys + features_keys)
+		local_id = 1
+		for tree in data:
+			# main fields
+			fields = [local_id] + [tree[key] for key in keys[1:] ]
+			# features
+			features = [tree["features"][key] for key in features_keys]
+			self.tree_table.appendRow(fields + features)
+			local_id += 1
+
 	def QueryCurrentArea(self):
 		rad = self.raduisOp[0].eval()
 		lat = self.pinpointOp[1].eval()
 		lon = self.pinpointOp[0].eval()
-		return self.QueryArea(lat, lon, rad)
+		response = self.QueryArea(lat, lon, rad)
+		if len(response) > 0:
+			self.processResponse(response)
+		else:
+			op.log.Error(f"No trees returned from query to {lat}, {lon} with radius {rad}")
 
 	def GetSelectedTrees(self):
 		response = requests.get(self.selectedTreesUrl)
---
+++ applications/touch/app/scenes/match/getGoogleImage.py
@@ -0,0 +1,55 @@
+# me - this DAT.
+# 
+# dat - the changed DAT
+# rows - a list of row indices
+# cols - a list of column indices
+# cells - the list of cells that have changed content
+# prev - the list of previous string contents of the changed cells
+# 
+# Make sure the corresponding toggle is enabled in the DAT Execute DAT.
+# 
+# If rows or columns are deleted, sizeChange will be called instead of row/col/cellChange.
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+""" Signs a URL using a URL signing secret """
+import requests
+
+switch_op = op("switch1")
+tree_edit_op = op("tree_image_edit")
+
+
+def onTableChange(dat):
+	# check if an image exists
+	has_image = "https://maps.googleapis.com/maps/api/streetview/metadata?location={},{}&key={}".format(dat[1, "latitude"], dat[1, "longitude"], op.env.Get('GOOGLE_KEY'))
+	signed_has_image = op.utils.SignUrl(has_image)
+	
+
+	response = requests.get(signed_has_image)
+	if response.status_code == 200:
+		status = response.json()["status"]
+		image_ok = status == "OK"
+		if image_ok:
+			get_image = "https://maps.googleapis.com/maps/api/streetview?size=600x600&location={},{}&key={}".format(dat[1, "latitude"], dat[1, "longitude"], op.env.Get('GOOGLE_KEY'))
+			signed_get_image = op.utils.SignUrl(get_image)
+			tree_edit_op.par.file = signed_get_image
+			switch_op.par.index = 1
+		else:
+			switch_op.par.index = 0
+	else:
+		switch_op.par.index = 0
+
+	return
+
+def onRowChange(dat, rows):
+	return
+
+def onColChange(dat, cols):
+	return
+
+def onCellChange(dat, cells, prev):
+	return
+
+def onSizeChange(dat):
+	return
+	
