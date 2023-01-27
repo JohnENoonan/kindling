f4d591e379470658c37e6accf2d82a792bd26c91
johnenoonan <noonanjohn21@gmail.com>
2023-01-26 23:44:48 -0500

    added signed url helper

--- applications/touch/app/utils/ext.py
+++ applications/touch/app/utils/ext.py
@@ -12,6 +12,10 @@ import os
 import re
 import time
 from datetime import datetime
+import hashlib
+import hmac
+import base64
+import urllib.parse as urlparse
 
 
 
@@ -47,3 +51,36 @@ class ext:
 
 	def GetEpochTime(self):
 		return int(time.time())
+
+	def SignUrl(self, input_url):
+		""" Sign a request URL with a URL signing secret.
+			Usage:
+			from urlsigner import sign_url
+			signed_url = sign_url(input_url=my_url, secret=SECRET)
+			Args:
+			input_url - The URL to sign
+			secret    - Your URL signing secret
+			Returns:
+			The signed request URL
+  		"""
+
+		url = urlparse.urlparse(input_url)
+
+		# We only need to sign the path+query part of the string
+		url_to_sign = url.path + "?" + url.query
+
+		# Decode the private key into its binary format
+		# We need to decode the URL-encoded private key
+		decoded_key = base64.urlsafe_b64decode(str(op.env.Get("GOOGLE_SECRET")))
+
+		# Create a signature using the private key and the URL-encoded
+		# string using HMAC SHA1. This signature will be binary.
+		signature = hmac.new(decoded_key, str.encode(url_to_sign), hashlib.sha1)
+
+		# Encode the binary signature into base64 for use within a URL
+		encoded_signature = base64.urlsafe_b64encode(signature.digest())
+
+		original_url = url.scheme + "://" + url.netloc + url.path + "?" + url.query
+
+		# Return signed URL
+		return original_url + "&signature=" + encoded_signature.decode()
