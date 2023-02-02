bdb379219d22ca5b715f2bd396eec234cd1a2cc6
johnenoonan <noonanjohn21@gmail.com>
2023-02-01 23:38:31 -0500

    added connected and tree_id

--- applications/touch/app/analytics/analyticsExt.py
+++ applications/touch/app/analytics/analyticsExt.py
@@ -1,15 +1,12 @@
 """
 This class serves as a helper for writing out the analytics for a user session
-
-uid, starttime, endtime, duration, created painting, viewed gallery, paintingpath
-
 """
 import os
 from datetime import datetime
 
 class AnalyticsExt:
 
-	header = "uid,starttime,endtime,duration"
+	header = "uid,starttime,endtime,duration,connected,treeid"
 
 	def __init__( self, ownerComp ):
 		self.folder = str(parent().par.Analyticsfolder)
@@ -23,14 +20,14 @@ class AnalyticsExt:
 	def datetimeToStr(self, timeobj):
 		return timeobj.strftime( "%y/%m/%d %H:%M:%S" )
 
-	def AddSession(self, uid, startitme, endtime):
+	def AddSession(self, uid, startitme, endtime, connected, treeid=-1):
 		"""
 		Append this user session to the analytics file
 		"""
 		try:
 			with open(self.outfilepath, 'a+', encoding='utf-8') as outfile:
 				dur = (endtime - startitme).total_seconds()
-				session = f"{int(uid)},{self.datetimeToStr(startitme)},{self.datetimeToStr(endtime)},{dur}\n"
+				session = f"{int(uid)},{self.datetimeToStr(startitme)},{self.datetimeToStr(endtime)},{dur},{connected},{treeid}\n"
 				outfile.write(session)		
 		except FileNotFoundError:
 			op.log.Error("anayltics file '{}'".format(self.outfilepath))
