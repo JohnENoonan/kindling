5f88d4d9bc2e6596b89bac5de7b9c852283ee2c5
johnenoonan <noonanjohn21@gmail.com>
2023-02-04 18:54:16 -0500

    added pin reset

--- applications/touch/app/touch/touchExt.py
+++ applications/touch/app/touch/touchExt.py
@@ -1,11 +1,17 @@
 
-
 class TouchExt:
 
 	def __init__(self, ownerComp):
 		self.ownerComp = ownerComp
 
-		self. pan_speed_op = op("speed1")
+		self.pan_speed_op = op("speed1")
+		self.map_data_op = op("map_data")
 
 	def ResetPan(self):
 		self.pan_speed_op.par.reset.pulse()
+
+	def ResetPin(self):
+		self.map_data_op.par.value2.val = .5
+		self.map_data_op.par.value3.val = .5
+		
+		
