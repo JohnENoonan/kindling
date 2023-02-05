4b9abfb700b8941d6cd027fe57e7aad2cc68610c
johnenoonan <noonanjohn21@gmail.com>
2023-02-04 19:52:26 -0500

    started animating swipes

---
+++ applications/touch/app/scenes/match/matchExt.py
@@ -0,0 +1,13 @@
+
+
+class MatchExt:
+
+	def __init__(self, ownerComp):
+		self.ownerComp = ownerComp
+
+
+	def SwipeRight(self):
+		op.log.Debug("swipe right")
+
+	def SwipeUp(self):
+		op.log.Debug("swipe up")
---
+++ applications/touch/app/scenes/match/swipeExecute.py
@@ -0,0 +1,28 @@
+# me - this DAT
+# 
+# channel - the Channel object which has changed
+# sampleIndex - the index of the changed sample
+# val - the numeric value of the changed sample
+# prev - the previous sample value
+# 
+# Make sure the corresponding toggle is enabled in the CHOP Execute DAT.
+
+def onOffToOn(channel, sampleIndex, val, prev):
+	if channel.name == "swipe_up":
+		op.match.ext.matchExt.SwipeUp()
+	elif channel.name == "swipe_right":
+		op.match.ext.matchExt.SwipeRight()
+	return
+
+def whileOn(channel, sampleIndex, val, prev):
+	return
+
+def onOnToOff(channel, sampleIndex, val, prev):
+	return
+
+def whileOff(channel, sampleIndex, val, prev):
+	return
+
+def onValueChange(channel, sampleIndex, val, prev):
+	return
+	
