11c153151f14ad83ced478810f9ddabd9fda3c84
johnenoonan <noonanjohn21@gmail.com>
2023-02-10 14:34:57 -0500

    log on reset map

--- applications/touch/app/scenes/map/mapExt.py
+++ applications/touch/app/scenes/map/mapExt.py
@@ -7,6 +7,7 @@ class MapExt:
 		self.zoom_slider_op = op("zoom_slider_geo")
 
 	def ResetMap(self):
+		op.log.Verbose("Reset Map")
 		self.area_slider_op.par.Value = .5
 		self.zoom_slider_op.par.Value = 0.0
 		op.touch.ResetPan()
